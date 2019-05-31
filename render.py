f76b42761fb193d19218d2aba90c17caaa312c95
David Owusu-Antwi <dowusu@mit.edu>
2019-05-30 20:16:00 -0400

    added pyqt rendering file

---
+++ render.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+With PyQt, this will take grids from
+  main.py and render a grid of rectangles
+  to the screen.
+"""
+
+import PyQt4
+
+## 1. Initialize and open PyQt window
+## 2. Draw rectangle to screen
+## 3. Change rectangle color
+
+if __name__ == "__main__":
+    pass
+
