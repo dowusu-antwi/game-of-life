b69b62563278ba25b99e0acced01aeb81be2c9e2
David Owusu-Antwi <dowusu@mit.edu>
2019-05-30 22:11:15 -0400

    added drawing of rectangles to the screen

--- render.py
+++ render.py
@@ -17,18 +17,46 @@ class App(QtGui.QMainWindow):
     This is the main widget class
     """
 
-    def __init__(self):
+    def __init__(self, width, height):
         self.app = QtGui.QApplication(sys.argv)
         super(App,self).__init__()
-        self.create_window()
+        self.setWindowTitle("Game of Life")
+        self.resize_window(width, height)
         self.show()
 
-    def create_window(self):
-        self.setWindowTitle("Game of Life")
-        self.setGeometry(0,0,100,100)
+    def resize_window(self, width, height):
+        """
+        This changes the window dimensions
+        """
+        self.setGeometry(0,0,width,height)
 
+    def paintEvent(self, e):
+        """
+        This method is required for PyQt to paint
+         elements to the screen. 
+        """
+
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        self.drawObjects(painter)
+        painter.end()
+
+    def drawObjects(self, painter):
+        """
+        This will draw any objects necessary to
+         the screen.
+        """
+
+        self.drawRectangles(painter)
+
+    def drawRectangles(self, painter):
+        """
+        This draws rectangles to the screen.
+        """
+        painter.drawRect(0,0,100,100)
+        painter.drawRect(100,100,100,100)
 
 if __name__ == "__main__":
 
-    new_widget = App()
+    new_widget = App(500, 250)
     sys.exit(new_widget.app.exec_())
